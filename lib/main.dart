548ea85f9a9d7f507080a2d515adc4e4cd05d3d0
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-10-29 13:10:08 +0500

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+//packages import
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+//local imports
 import 'package:flutter/material.dart';
 import 'package:instagram_app/data/colors.dart';
 import 'package:instagram_app/Screens/home_screen.dart';
 import 'package:instagram_app/Screens/web_screen.dart';
 import 'package:instagram_app/Screens/mobile_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
