41fa48de3aef85d0e5c2657bc13383e45efa8a8b
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-11-02 13:50:28 +0500

    Reverted to previous version

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:instagram_app/data/colors.dart';
+import 'package:instagram_app/resources/auth_method.dart';
+import 'package:instagram_app/resources/snackbar_function.dart';
 import 'package:instagram_app/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,6 +13,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var _isLoading = false;
+  final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
@@ -19,6 +23,37 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+  }
+
+// login user method
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    if (_formKey.currentState!.validate()) {
+      final res = await AuthMethods().loginUser(
+        email: _emailController.text,
+        password: _passwordController.text,
+        scaffoldMessengerState: ScaffoldMessenger.of(context),
+      );
+      debugPrint('errrrrrrrrrrrrrrrror');
+      debugPrint(res);
+      if (res == 'success') {
+        setState(() {
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+        if (mounted) {
+          showSnackBar(
+            content: res,
+            context: context,
+          );
+        }
+      }
+    }
   }
 
   @override
@@ -50,7 +85,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     !value.contains('@')) {
                   return 'invalid email address.';
                 }
-                return null;
+                return '';
               },
             ),
             const SizedBox(height: 20),
@@ -66,20 +101,27 @@ class _LoginScreenState extends State<LoginScreen> {
                 } else if (value.length < 8) {
                   return 'Password must be at least 8 characters long.';
                 }
-                return null;
+                return '';
               },
             ),
             const SizedBox(height: 20),
             //login button
-            Container(
-              width: double.infinity,
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
-                color: blueColor,
-              ),
-              child: const Text('Log in'),
+            InkWell(
+              onTap: () {
+                loginUser();
+              },
+              child: _isLoading
+                  ? const CircularProgressIndicator()
+                  : Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        color: blueColor,
+                      ),
+                      child: const Text('Log in'),
+                    ),
             ),
             const SizedBox(
               height: 12,
--- lib/resources/auth_method.dart
+++ lib/resources/auth_method.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:instagram_app/resources/snackbar_function.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -13,20 +12,21 @@ class AuthMethods {
     required String password,
     required String username,
     required String fullname,
-    required ScaffoldMessengerState scaffoldMessengerState,
   }) async {
-    String res = 'Some error occured';
+    print('signup of auth method is opened');
+    String res = 'Some error occurred';
     try {
-      if (email.isNotEmpty ||
-          password.isEmpty ||
-          username.isNotEmpty ||
+      if (email.isNotEmpty &&
+          password.isNotEmpty &&
+          username.isNotEmpty &&
           fullname.isNotEmpty) {
-        // register user
+        // Register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
-        // add user to database
+        print(cred);
+        // Add user to the database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'fullname': fullname,
           'username': username,
@@ -37,13 +37,38 @@ class AuthMethods {
         });
         res = 'Success';
       }
+      print('sign up method completed');
     } on FirebaseAuthException catch (error) {
-      res = error.toString();
+      print('sign up method failed error: $error');
+      res =
+          error.message.toString(); // Update this line to get the error message
+    } catch (e) {
+      print('Unknown error: $e');
+    }
+    print('error message sent');
+    return res;
+  }
 
-      showSnackBar(
-        content: res,
-        scaffoldMessengerState: scaffoldMessengerState,
+// Login user
+  Future<String> loginUser({
+    required String email,
+    required String password,
+    required ScaffoldMessengerState scaffoldMessengerState,
+  }) async {
+    String res = 'Some error occurred';
+    try {
+      // login user
+      UserCredential cred = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
       );
+      if (cred.user != null) {
+        res = 'Success';
+      } else {
+        res = 'User login failed.';
+      }
+    } on FirebaseAuthException catch (e) {
+      res = e.toString();
     }
     return res;
   }
